9f8fd0dde7daa7e4ea7dbf70cc2bc9fa606c442e
JayCeeCreates <gagaring.cedric@gmail.com>
2021-05-11 00:17:17 +0000

    Update install-arch-deps.sh

--- install-arch-deps.sh
+++ install-arch-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S xorg xorg-server picom feh dunst acpi pcmanfm ttf-font-awesome firefox lxappearance neofetch xarchiver zsh zsh-autosuggestions zsh-syntax-highlighting --noconfirm --needed
+sudo pacman -S xorg xorg-server picom feh dunst acpi pcmanfm notification-daemon ttf-font-awesome firefox lxappearance neofetch xarchiver zsh zsh-autosuggestions zsh-syntax-highlighting --noconfirm --needed
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.p10k
